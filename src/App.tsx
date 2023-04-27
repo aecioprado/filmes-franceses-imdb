bfc2cbc091904f5b9acf1c5840a638101c294925
aecioprado <aecioprado@gmail.com>
2023-04-27 11:24:24 -0300

    reorganizacao da estrutura de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css'
 import { useEffect, useState } from 'react';
 import React from 'react';
 import axios from 'axios';
@@ -22,16 +22,19 @@ function App() {
   }, [])
 
   return (
+    <div>
+      <div className='container-fluif bg-primary'>container</div>
     <ul>
       {repositorios.map(repo => {
         return (
           <li key={repo.full_name}>
             <strong>{repo.full_name}</strong>
             <p>{repo.description}</p>
-            </li>
+          </li>
         )
       })}
     </ul>
+  </div>
   );
 }
 
